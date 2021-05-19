a5cd813f56901a848a9a2e5bd42900bf8beb7977
nischalvarasada <npvarasada@hotmail.com>
2021-05-19 16:34:27 +0530

    Updated nugert packages..

--- HeroBot/Controllers/CallUserTokenController.cs
+++ HeroBot/Controllers/CallUserTokenController.cs
@@ -1,10 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Communication;
+using Azure.Communication.Identity;
+using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
 namespace HeroBot.Controllers
 {
-    public class CallUserTokenController
+    [Route("call")]
+    public class CallUserTokenController: Controller
     {
-        public CallUserTokenController()
+        private readonly CommunicationIdentityClient _client;
+
+        public CallUserTokenController(IConfiguration configuration)
         {
+            _client = new CommunicationIdentityClient(configuration["ResourceConnectionString"]);
         }
+
+        /// <summary>
+        /// Gets a token to be used to initalize the call client
+        /// </summary>
+        /// <returns></returns>
+        [Route("token")]
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            try
+            {
+                //Response<(CommunicationUserIdentifier User, AccessToken Token)> response = await _client.CreateUserWithTokenAsync(scopes: new[] { CommunicationTokenScope.VoIP });
+                var response = await _client.CreateUserAndTokenAsync(scopes: new[] { CommunicationTokenScope.VoIP });
+
+                var responseValue = response.Value;
+
+                var jsonFormattedUser = new
+                {
+                    communicationUserId = responseValue.User.Id
+                };
+
+                var clientResponse = new
+                {
+                    user = jsonFormattedUser,
+                    //token = responseValue.Token.Token,
+                    token = responseValue.AccessToken.Token,
+                    //expiresOn = responseValue.Token.ExpiresOn
+                    expiresOn = responseValue.AccessToken.ExpiresOn
+                };
+
+                return this.Ok(clientResponse);
+            }
+            catch (RequestFailedException ex)
+            {
+                Console.WriteLine($"Error occured while Generating Token: {ex}");
+                return this.Ok(this.Json(ex));
+            }
+        }   
     }
 }
